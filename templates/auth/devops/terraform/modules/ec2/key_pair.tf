67f9a7b0e6599b43b3936edeed3d8818b79f4fbe
James Miller Blog <hi@jamesmiller.blog>
2023-07-09 14:25:43 +0100

    feat: remove auth

--- templates/auth/devops/terraform/modules/ec2/key_pair.tf
+++
@@ -1,9 +0,0 @@
-# resource "tls_private_key" "this" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
-
-# resource "aws_key_pair" "this" {
-#   key_name   = var.key_name
-#   public_key = tls_private_key.this.public_key_openssh
-# }
